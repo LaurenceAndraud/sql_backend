b9e47d019c4168b9e5510465b92786651ce1619a
LaurenceAndraud <laurence.andraud96@gmail.com>
2025-01-21 15:20:38 +0100

    fix bug for dashboard

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from scripts.queries import QUERIES
 
 engine = create_engine('sqlite:///RetailSalesDB.db')
@@ -8,9 +8,14 @@ engine = create_engine('sqlite:///RetailSalesDB.db')
 st.title("Tableau de bord des ventes au détail")
 
 with engine.connect() as connection:
-    total_revenue = connection.execute(QUERIES["total_revenue"]).fetchone()[0]
-st.metric("Chiffre d'affaires total", f"{total_revenue:,.2f} €")
-category_revenue = pd.read_sql(QUERIES["category_revenue"], engine)
-st.bar_chart(category_revenue.set_index("ProductCategory")["Revenue"])
-monthly_revenue = pd.read_sql(QUERIES["monthly_revenue"], engine)
-st.line_chart(monthly_revenue.set_index("Month")["MonthlyRevenue"])
+    total_revenue_query = text(QUERIES["total_revenue"])
+    total_revenue = connection.execute(total_revenue_query).fetchone()[0]
+    st.metric("Chiffre d'affaires total", f"{total_revenue:,.2f} €")
+
+    category_revenue_query = text(QUERIES["category_revenue"])
+    category_revenue = pd.read_sql(category_revenue_query, engine)
+    st.bar_chart(category_revenue.set_index("ProductCategory")["Revenue"])
+
+    monthly_revenue_query = text(QUERIES["monthly_revenue"])
+    monthly_revenue = pd.read_sql(monthly_revenue_query, engine)
+    st.line_chart(monthly_revenue.set_index("Month")["MonthlyRevenue"])
--- scripts/create_db.py
+++ scripts/create_db.py
@@ -1,16 +1,30 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import pandas as pd
 
 # Load data
-data = pd.read_csv("../retail_sales_dataset.csv")
+data = pd.read_csv("retail_sales_dataset.csv")
+
+data.rename(columns={
+    "Transaction ID": "TransactionID",
+    "Date": "TransactionDate",
+    "Customer ID": "CustomerID",
+    "Gender": "Gender",
+    "Age": "Age",
+    "Product Category": "ProductCategory",
+    "Quantity": "Quantity",
+    "Price per Unit": "PricePerUnit",
+    "Total Amount": "TotalAmount"
+}, inplace=True)
+
+data["TransactionDate"] = pd.to_datetime(data["TransactionDate"], errors="coerce").dt.date
 
 # Connect to the sqlite base
 engine = create_engine("sqlite:///RetailSalesDB.db")
 
 # Execut query
 with engine.connect() as connection:
-    connection.execute("""
-    CREATE TABLE RetailSales (
+    connection.execute(text("""
+    CREATE TABLE IF NOT EXISTS RetailSales (
         TransactionID INT PRIMARY KEY,
         TransactionDate DATE,
         CustomerID VARCHAR(20),
@@ -21,7 +35,7 @@ with engine.connect() as connection:
         PricePerUnit DECIMAL(10, 2),
         TotalAmount DECIMAL(10, 2)
     );
-    """)
+    """))
 
 # Insert data
 data.to_sql("RetailSales", con=engine, if_exists='append', index=False)
